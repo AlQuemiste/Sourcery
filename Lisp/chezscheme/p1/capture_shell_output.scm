98d16ff9aa7e12a30841ca822509b95fe05de3b1
Ammar Nejati <a.nejati@fz-juelich.de>
2023-04-02 14:12:51 +0200

    Lisp: add Scheme code to capture shell output

---
+++ Lisp/chezscheme/p1/capture_shell_output.scm
@@ -0,0 +1,10 @@
+(import (rnrs)) ; imports the R6RS standard library
+
+(define (run-shell-command command)
+  (call-with-input-string
+    (open-input-pipe command)
+    get-string-all))
+
+(define result (run-shell-command "ls -l"))
+
+(display result)
