1bebd0cda7c607ac129fc68c39ddb94745d0588c
AlQuemist <alquemist@Lyriks>
2023-10-25 12:18:40 +0200

    CMake: extend 'main.c' to load a shared library at runtime

--- cmake/cmake_minimal/main.c
+++ cmake/cmake_minimal/main.c
@@ -1,9 +1,53 @@
 #include <stdio.h>
+#include <dlfcn.h>
+
 #include "foo.h"
- 
+#include "qux.h"
+
+int result(void)
+{
+    // load the shared library
+    void *handle;
+    handle = dlopen("libqux.so", RTLD_LAZY);
+    if (!handle) {
+        fprintf(stderr, "%s\n", dlerror());
+        return -1;
+    }
+
+    // get a pointer to the function
+    int (*function)(void);
+    char *error;
+    function = dlsym(handle, "qux");
+    error = dlerror();
+    if (error != NULL) {
+        fprintf(stderr, "%s\n", error);
+        return 1;
+    }
+
+    // call the function
+    const int res = function();
+
+    // unload the shared library
+    if (handle)
+        dlclose(handle);
+
+    return res;
+}
+
 int main(void)
 {
     puts("This is a shared library test...");
-    foo();
+    int number;
+    printf("> Enter an integer: ");
+    scanf("%d", &number);
+    printf("Selection: %d\n", number);
+    if (number == 1) {
+        foo();
+    } else {
+        int res = result();
+        printf("> result: %d\n", res);
+    }
+
+    puts("--END--");
     return 0;
 }
