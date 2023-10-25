b01bb7bf827c6b9acd263cfdb4230d690ea66023
AlQuemist <alquemist@Lyriks>
2023-10-25 12:19:11 +0200

    CMake: adapt the build shell-script to the latest changes

--- cmake/cmake_minimal/mk.sh
+++ cmake/cmake_minimal/mk.sh
@@ -1,11 +1,25 @@
-echo compile foo.c
-gcc -c -Wall -Werror -fPIC foo.c
+CC=gcc
 
-echo make shared library
-gcc -shared -Wl,-soname,libfoo.so -o libfoo.so foo.o
-#gcc -Wall -L‍/home/utopia/Documents/shlib/ -lfoo -o test main.c 
+build_dir=./bld/
+foolibname=libfoo.so
+quxlibname=libqux.so
+exename=test
 
-echo make exe
-gcc -L./  -Wl,-rpath='$ORIGIN' -lfoo -Wall -o test main.c
+C_FLAGS=$C_FLAGS
+mkdir -p $build_dir
 
-# SONAME is needed at run time. At compilation time, only a linker name or real name is needed 
+echo "* compile..."
+$CC -c $C_FLAGS -o $build_dir/foo.o foo.c
+$CC -c $C_FLAGS -o $build_dir/qux.o qux.c
+$CC -c $C_FLAGS -o $build_dir/$exename.o main.c
+
+echo "* make shared library $foolibname..."
+$CC -shared -fPIC -Wl,-soname,$foolibname -o $build_dir/$foolibname  $build_dir/foo.o
+
+echo "* make shared library $quxlibname..."
+$CC -shared -fPIC -Wl,-soname,$quxlibname -o $build_dir/$quxlibname  $build_dir/qux.o
+
+echo "* make exe..."
+$CC  -Wl,-rpath='$ORIGIN' -o $build_dir/$exename $build_dir/$exename.o -L$build_dir -lfoo
+
+# SONAME is needed at run time. At compilation time, only a linker name or real name is needed
