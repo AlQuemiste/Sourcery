c1dd9299972a0d78f9adb2cb1f59e5e149ba0dc8
AlQuemist <alquemist@Lyriks>
2024-07-11 15:47:54 +0200

    C++:SWIG: improve the example and add a Python test script

--- Cpp/pySWIG/example.h
+++ Cpp/pySWIG/example.h
@@ -2,14 +2,28 @@
 
 #include <memory>
 
+typedef double value_t;
+
+struct FooArray {
+    int size;
+    value_t* array_ptr;
+};
+
 class Foo
 {
-public:
+public:   
+    int size = 0;
+    value_t* array_ptr = nullptr;
+    
     Foo();
     ~Foo();
-    void say();
-
+    Foo(const int sz);
+    
+    void say() const;
     void store(Foo* foo_ptr);
+    // neeed for interfacing C-arrays
+    struct FooArray array();
+    // void array(int* sz, value_t** a_ptr);
 
 #ifndef SWIG
     std::unique_ptr<Foo> clone();
@@ -19,4 +33,4 @@ private:
     std::unique_ptr<Foo> _ptr;
 };
 
-int fact(int n);
+long int fact(int n);
