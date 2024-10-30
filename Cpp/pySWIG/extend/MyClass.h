92a67cd905a1b40f85b5f3099d32226978fd362d
Ammar Nejati <a.nejati@fz-juelich.de>
2024-10-30 21:50:07 +0100

    C++:SWIG: add Foo and SWIG_FooWrapper class to use a Python class in C++

--- Cpp/pySWIG/extend/MyClass.h
+++ Cpp/pySWIG/extend/MyClass.h
@@ -1,13 +1,19 @@
+class Foo;
+
 class MyClass {
 public:
     int i = 0;
     // Python function
     void* pyFunc = nullptr;
 
+    MyClass();
+
     // externally defined function which prints the Python function name
     void (*pyGetName)(void*) = nullptr;
 
     void name();
+
+    int calcFoo(Foo& foo, const int i);
 
 #ifndef SWIG
     void configure(void* pFunc_, const int i_);
@@ -20,4 +26,16 @@ public:
 struct Info {
     int i = 0;
     double x = 0;
+};
+
+class Foo {
+public:
+    Foo();
+    Foo(const int i_);
+    virtual ~Foo();
+
+    virtual int calc(const int j);
+
+private:
+    const int i;
 };
--- Cpp/pySWIG/extend/test_MyClass.py
+++ Cpp/pySWIG/extend/test_MyClass.py
@@ -20,6 +20,10 @@ print()
 print("Python> print MyClass repr:")
 print(my_class)
 
+print("Python> MyClass SWIG init:")
+my_class_2 = my.MyClass(1, 2)
+print(my_class_2)
+
 print("Python> instantiate Info:")
 info = my.Info()
 info.i = 42; info.x = -3.14
@@ -27,5 +31,22 @@ info.i = 42; info.x = -3.14
 foo = lambda info_: print("Python> Info (i = {:d}, x = {:f})".format(info_.i, info_.x))
 
 my_class.callPyFunction(foo, info)
+
+print()
+print("Python> test FooWrapper")
+
+foo = my.Foo(1)
+r1 = foo.calc(-1)
+print("Python> result_1 =", r1)
+
+r2 = my_class.calcFoo(foo, -1)
+print("Python> result_2 =", r2)
+
+class MyFoo(my.Foo):
+    def calc(i:int):
+        return i + 2
+
+r3 = my_class.calcFoo(MyFoo, -2)
+print("Python> result_3 =", r3)
 
 print("Python> TEST MyClass: done.")
